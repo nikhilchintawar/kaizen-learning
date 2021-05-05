39baef57cc97eff3388d0f2a041b2fb25e17d0a5
Nikhil Chintawar <chintawarnikhil88@gmail.com>
2021-05-05 08:06:06 +0530

    Add through model for course creator

--- kaizen/api/courses/admin.py
+++ kaizen/api/courses/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from api.courses.models.courses import Courses
 from api.courses.models.course_lessons import Course_Lessons
-from api.courses.models.courses_author import Course_Author
+from api.courses.models.course_creator import Course_Creator
 
 # Register your models here.
 
 admin.site.register(Courses)
 admin.site.register(Course_Lessons)
-admin.site.register(Course_Author)
+admin.site.register(Course_Creator)
--- kaizen/api/courses/models/course_creator.py
+++ kaizen/api/courses/models/course_creator.py
@@ -4,9 +4,9 @@ from .courses import Courses
 from api.author.models import Author
 
 
-class Course_Author(models.Model):
+class Course_Creator(models.Model):
     course = models.ForeignKey(Courses, on_delete=models.PROTECT)
-    authors = models.ForeignKey(Author, on_delete=models.CASCADE, db_index=True)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.course}-{self.authors}"
